de5257cb6cf246d8fe27b8a2a0bca23a8614f6d2
Malte Isberner <mi@stackrox.com>
2019-05-21 20:00:23 +0200

    delete payload

--- pkg/payload/response.go
+++
@@ -1,8 +0,0 @@
-package payload
-
-// AuthorizationResponse encapsulates a response from the authorization plugin.
-type AuthorizationResponse struct {
-	// AuthorizedScopes are the scopes for which authorization is granted. All other requested scopes that are not
-	// listed here (or subsumed by the scopes returned here) are treated as denied.
-	AuthorizedScopes []AccessScope
-}
